1ba811d0a00aebe2ecf37fb716bfb15e2568f8c3
Ashique Kainote <ashique.kainote@gmail.com>
2023-04-08 16:19:33 +0530

    second

---
+++ app.js
@@ -0,0 +1,67 @@
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
+
+
+client.initialize();
+
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('authenticated', () => {
+  console.log('AUTHENTICATED');
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
+});
+
+client.on('message', async (message) => {
+  if (message.body === 'hello') {
+    message.reply('Hiiiii');
+  }
+
+  if (message.body === 'who are you') {
+    //get media from url
+    const media = await MessageMedia.fromUrl(
+      'https://quteezwear.web.app/images/asq.jpg'
+    );
+
+    //replying with media
+    client.sendMessage(message.from, media, {
+      caption: 'This is me',
+    });
+  }
+
+  if(message.body === 'GL01')
+  {
+    //get media from url
+    const media = await MessageMedia.fromUrl(
+      'https://quteezwear.web.app/images/03.png'
+    );
+
+    //replying with media
+    client.sendMessage(message.from, media, {
+      caption: 'Green Organsa',
+    });
+  }
+
+  if(message.body === 'BY01')
+  {
+    //get media from url
+    const media = await MessageMedia.fromUrl(
+      'https://quteezwear.web.app/images/01.png'
+    );
+
+    //replying with media
+    client.sendMessage(message.from, media, {
+      caption: 'Boys Tees',
+    });
+  }
+
+});
+
